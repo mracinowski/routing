55e46957bc35a1204518c79f864d75a7b59e3624
Stanisław Czech <sc@lo3gdynia.pl>
2025-01-19 15:29:30 +0100

    some code

--- worker/main.py
+++ worker/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from common import fileOperations
+import uuid
 
 app = FastAPI()
 
@@ -11,14 +12,37 @@ isAuthoritative = True
 
 
 data = []
-dataLock = ""
+
+class localData:
+    # Lock of the current state of the data
+    dataLock = ""
+    
+    # Preprocessed data for passthrough
+    passthroughMatrix = []
+    
+    # List of internal nodes
+    internalNodes = []
+    
+    # List of external node
+    externalNodes = []
+    
+    # List of all edges
+    edges: dict[str, list[str, str]] = {}
+    
+    
+data = localData()
 
 # For non authoritative, check if there is new data stored about the region
 # If there is update, refresh in memory data
 def refreshData():
-    if not fileOperations.checkLock("path/To/Lock", dataLock):
+    if not fileOperations.checkLock("path/To/Lock", data.dataLock):
         return
     # Load new data into memory
+
+
+# Save the current state of data to the drive
+def saveData():
+    pass
 
 @app.get("/getStatus")
 def getStatus():
@@ -26,7 +50,7 @@ def getStatus():
     Get some kind of id of the current state of the network
     We don't want to send the internal data, if it hasn't changed
     """
-    return "Hello world!"
+    return data.dataLock
 
 @app.get("/getPassthroughData/{lastId}")
 def getPassthroughData(lastId: str):
@@ -42,6 +66,12 @@ def getPassthroughData(lastId: str):
         return res
     
     res['hasData'] = True
+    res['lock'] = getStatus()
+    res['data'] = {}
+    res['data']['matrix'] = data.passthroughMatrix
+    # List of nodes in the same order as in the matrix
+    res['data']['nodes'] = [] 
+    return res
 
 @app.get("/getInternalConnection/{internalNode1}/{internalNode2}")
 def getInternalConnection(internalNode1: str, internalNode2: str):
@@ -57,15 +87,68 @@ def getDistancesMatrix(internalNode1: str):
     and will use preprocessed data to answer this query. 
     """
 
+# Do we need any locks in this code? 
+# TBF, I don't remember how python handles parallel code ~SC
+
 @app.put("/addEdge/{v1}/{v2}/{distance}")
 def addEdge(v1: str, v2: str, distance: int):
     """
     Adds an edge between v1 and v2 internal nodes with the given distance.
     Returns the internal id of the created path
     """
+    if isAuthoritative == False:
+        raise HTTPException(403, "This node cannot edit data")
+    
+    # Data checks
+    for i in [v1, v2]:
+        if i not in data.internalNodes and i not in data.externalNodes:
+            raise HTTPException(400, "Invalid node id")
+        
+    
+    edgeUUID = uuid.uuid4()
+    if data.edges[v1] is None:
+        data.edges[v1] = []
+    
+    data.edges[v1].insert([v2 ,edgeUUID])
+    data.edges[v2].insert([v1 ,edgeUUID])
+    
+    # TODO: Update preprocessed data
+    
+    return {'status':'Ok'}
     
 @app.delete("/deleteEdge/{id}/")
 def deleteEdge(id: str):
     """
     Deletes edge with the given id
     """
+    if isAuthoritative == False:
+        raise HTTPException(403, "This node cannot edit data")
+
+@app.post("/setNodeStatus/{id}/{status}/")
+def setNodeStatus(id: str, status: str):
+    """
+    Mark the node as either internal or external
+    """
+    if isAuthoritative == False:
+        raise HTTPException(403, "This node cannot edit data")
+    
+    
+    if status == "internal":
+        newType = False
+    elif status == "external":
+        newType = True
+    else:
+        raise HTTPException(400, "New status is invalid: expected one of [internal, external]")
+    
+    if ((newType == False and id in data.internalNodes) or
+        (newType == True and id in data.externalNodes)):
+        return {'status': 'Ok', 'message': 'No data was changed'}
+    
+    if (newType == False and id in data.externalNodes):
+        pass #TODO: Update preprocessed data
+    elif (newType == True and id in data.internalNodes):
+        pass #TODO: Update preprocessed data
+    else:
+        raise HTTPException(400, "Invalid node ID")
+    
+    return {'status': 'Ok'}
