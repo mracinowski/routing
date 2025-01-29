b65f1fae94bae59db30176101acf60d4246b8879
JOpalski <jo448415@students.mimuw.edu.pl>
2025-01-29 23:42:05 +0100

    Added first tests for distance finding

--- common/graph.py
+++ common/graph.py
@@ -5,7 +5,7 @@ class edge:
     n2: str
     id: str
     length: int
-    
+
     def __init__(self, _n1, _n2, _id, _length):
         self.n1 = _n1
         self.n2 = _n2
@@ -18,8 +18,11 @@ class ResultSet1:
         self.res = {}
         
     def callback(self, node: str, dist: int, _from: str):
+        if node in self.res and self.res[node] <= dist:
+            return False
         self.res[node] = dist
-    
+
+
 class PathResult:
     
     def __init__(self, source, target):
@@ -27,12 +30,12 @@ class PathResult:
         self.source = source
         self.target = target
         self.dist = 0
-        
+
     def callback(self, node: str, dist: int, from_: str):
         self.paths[node] = from_
         if self.target == node:
             self.dist = dist
-        
+
     def compute(self):
         res = []
         res.append(self.target)
@@ -41,16 +44,16 @@ class PathResult:
         return res
 
 # Does a bfs, starting from starting node, and calls callback on each visited node
-def bfs(starting: str, edges: dict[str, list[edge]], callback: Callable[[str, int, str]]):
+def bfs(starting: str, edges: dict[str, list[edge]], callback: Callable[[str, int, str], bool]):
     visited = {}
-    queue: list[tuple[str, int]] = []
-    queue.append((starting, 0))
-    visited[starting] = True
+    queue: list[tuple[str, int]] = [(starting, 0)]
+    # visited[starting] = True # it's a str, later we're putting edges?
     while len(queue) > 0:
         (element, dist) = queue.pop(0)
-        callback(element, dist)
+        if not callback(element, dist, starting):
+            continue
         for edge in edges[element]:
-            if visited[edge] == True:
+            if edge in visited:
                 continue
             visited[edge] = True
-            queue.append((edge.n2, dist + edge.length))    
+            queue.append((edge.n2, dist + edge.length))
