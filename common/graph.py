30afda3a62c8c7d2d6165be27aab1fe34e924b4d
Stanisław Czech <sc@lo3gdynia.pl>
2025-01-30 11:09:11 +0100

    Fixes

--- common/graph.py
+++ common/graph.py
@@ -18,10 +18,7 @@ class ResultSet1:
         self.res = {}
         
     def callback(self, node: str, dist: int, _from: str):
-        if (node in self.res) and self.res[node] <= dist:
-            return False
         self.res[node] = dist
-        return True
 
 
 class PathResult:
@@ -35,10 +32,7 @@ class PathResult:
     def callback(self, node: str, dist: int, from_: str):
         self.paths[node] = from_
         if self.target == node:
-            if self.dist is not None and self.dist < dist:
-                return False
             self.dist = dist
-        return True
 
     def compute(self):
         res = []
@@ -50,14 +44,17 @@ class PathResult:
 # Does a bfs, starting from starting node, and calls callback on each visited node
 def bfs(starting: str, edges: dict[str, list[edge]], callback: Callable[[str, int, str], bool]):
     visited = {}
-    queue: list[tuple[str, int]] = [(starting, 0)]
-    # visited[starting] = True # it's a str, later we're putting edges?
+    queue: list[tuple[int, str, str]] = [(0, starting, "")]
+    visited[starting] = 0
     while len(queue) > 0:
-        (element, dist) = queue.pop(0)
-        if not callback(element, dist, starting):
+        queue.sort()
+        (dist, element, from_) = queue.pop(0)
+        if visited[element] < dist:
             continue
+        callback(element, dist, from_)
         for edge in edges[element]:
-            if edge in visited:
+            if edge.n2 in visited.keys() and visited[edge.n2] <= dist + edge.length:
                 continue
-            visited[edge] = True
-            queue.append((edge.n2, dist + edge.length))
+            
+            visited[edge.n2] = dist + edge.length
+            queue.append((dist + edge.length, edge.n2, element))
