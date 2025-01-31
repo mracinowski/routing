de7189b4ef925fcad40543531642469d06d130d5
Jan Opalski <jo448415@students.mimuw.edu.pl>
2025-01-31 11:42:23 +0100

    Minor style changes

--- worker/main.py
+++ worker/main.py
@@ -29,22 +29,19 @@ isAuthoritative = True
 class localData(JSONEncoder):
 	# Lock of the current state of the data
 	dataLock = ""
-	
 	# Preprocessed data for passthrough
 	passthroughMatrix = []
-	
 	# List of internal nodes
 	internalNodes = []
-	
 	# List of external node
 	externalNodes = []
-	
 	# List of all edges
 	edges: dict[str, list[graph.Edge]] = {}
-	
-	
+
+
 data = localData()
 dataFile = "/some/path"
+lockFile = "path/to/lock"
 
 @app.on_event("startup")
 async def lease():
@@ -54,7 +51,7 @@ async def lease():
 # For non-authoritative, check if there is new data stored about the region
 # If there is update, refresh in memory data
 def refreshData():
-	if not fileOperations.checkLock("path/To/Lock", data.dataLock):
+	if not fileOperations.checkLock(lockFile, data.dataLock):
 		return
 	textData = fileOperations.readFile(dataFile)
 	jsonData = json.loads(textData, object_hook=lambda d: SimpleNamespace(**d))
@@ -102,34 +99,34 @@ def getPassthroughData(lastId: str):
 	and will use preprocessed data to answer this query. 
 	"""
 	res = {'status': 'Ok'}
-	
+
 	if lastId == getStatus():
 		res['hasData'] = False
 		return res
-	
+
 	res['hasData'] = True
 	res['lock'] = getStatus()
 	res['data'] = {}
 	res['data']['matrix'] = data.passthroughMatrix
 	# List of nodes in the same order as in the matrix
-	res['data']['nodes'] = data.externalNodes 
+	res['data']['nodes'] = data.externalNodes
 	return res
 
 @app.get("/getInternalConnection/{internalNode1}/{internalNode2}")
 def getInternalConnection(internalNode1: str, internalNode2: str):
 	"""
-	Returns distance and exact path between between two internal nodes
+	Returns distance and exact path between two internal nodes
 	"""
 	ensureExistingNode(internalNode1)
 	ensureExistingNode(internalNode2)
-	
+
 	graphPath = graph.PathResult(internalNode1, internalNode2)
 	graph.dijkstra(internalNode1, data.edges, graphPath.callback)
 
 	res = {'status': 'Ok'}
 	res['distance'] = graphPath.dist
 	res['path'] = graphPath.compute()
- 
+
 	return res
 
 @app.get("/getDistancesMatrix/{internalNode1}")
@@ -138,16 +135,15 @@ def getDistancesMatrix(internalNode1: str):
 	Returns distance from the internal node to all external connections.
 	"""
 	ensureExistingNode(internalNode1)
-	
+
 	graphRes = graph.ResultSet1()
 	graph.dijkstra(internalNode1, data.edges, graphRes.callback)
-	
-	res = {'status': 'Ok'}
-	res['data'] = graphRes.res
+
+	res = {'status': 'Ok', 'data': graphRes.res}
 
 	return res
 
-# Do we need any locks in this code? 
+# Do we need any locks in this code?
 # TBF, I don't remember how python handles parallel code ~SC
 
 @app.put("/addEdge/{v1}/{v2}/{distance}")
@@ -156,31 +152,31 @@ def addEdge(v1: str, v2: str, distance: int):
 	Adds an edge between v1 and v2 internal nodes with the given distance.
 	Returns the internal id of the created path
 	"""
-	if isAuthoritative == False:
+	if not isAuthoritative:
 		raise HTTPException(403, "This node cannot edit data")
-	
+
 	# Data checks
 	for i in [v1, v2]:
 		ensureExistingNode(i)
-	
+
 	edgeUUID = uuid.uuid4()
 	for i in [v1, v2]:
 		if data.edges[i] is None:
 			data.edges[i] = []
-	
+
 	data.edges[v1].insert(graph.Edge(v1, v2, edgeUUID, distance))
 	data.edges[v2].insert(graph.Edge(v2, v1, edgeUUID, distance))
-	
+
 	processPassthroughData()
-	
+
 	return {'status':'Ok', 'id': edgeUUID}
-	
+
 @app.delete("/deleteEdge/{id}/")
 def deleteEdge(id: str):
 	"""
 	Deletes edge with the given id
 	"""
-	if isAuthoritative == False:
+	if not isAuthoritative:
 		raise HTTPException(403, "This node cannot edit data")
 
 	for key in data.edges.keys():
@@ -189,42 +185,36 @@ def deleteEdge(id: str):
 				data.edges[key].remove(element)
 
 	processPassthroughData()
-	
-	return {'status':'Ok'}
+
+	return {'status': 'Ok'}
 
 @app.post("/setNodeStatus/{id}/{status}/")
 def setNodeStatus(id: str, status: str):
 	"""
 	Mark the node as either internal or external
 	"""
-	if isAuthoritative == False:
+	if not isAuthoritative:
 		raise HTTPException(403, "This node cannot edit data")
-	
-	
+
 	if status == "internal":
 		newType = False
 	elif status == "external":
 		newType = True
 	else:
 		raise HTTPException(400, "New status is invalid: expected one of [internal, external]")
-	
-	if ((newType == False and id in data.internalNodes) or
-		(newType == True and id in data.externalNodes)):
+
+	if ((newType is False and id in data.internalNodes) or
+		(newType is True and id in data.externalNodes)):
 		return {'status': 'Ok', 'message': 'No data was changed'}
-	
-	if (newType == False and id in data.externalNodes):
+
+	if newType is False and id in data.externalNodes:
 		data.externalNodes.remove(id)
-		data.internalNodes.insert(id)
-	elif (newType == True and id in data.internalNodes):
+		data.internalNodes.append(id)
+	elif newType is True and id in data.internalNodes:
 		data.internalNodes.remove(id)
-		data.externalNodes.insert(id)
+		data.externalNodes.append(id)
 	else:
 		raise HTTPException(400, "Invalid node ID")
 
 	processPassthroughData()
 	return {'status': 'Ok'}
-
-
-@app.get("/manager")
-async def get_manager():
-	return manager.__url
