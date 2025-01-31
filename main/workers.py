841242203653234c37cd34f7d65db43da1bc9a40
Michał Racinowski <m.racinowski@students.mimuw.edu.pl>
2025-01-31 19:21:35 +0100

    Add request method to Workers

--- main/workers.py
+++ main/workers.py
@@ -1,5 +1,6 @@
 from redis import Redis
 import logging
+import httpx
 
 log = logging.getLogger("uvicorn")
 
@@ -21,3 +22,14 @@ class Workers:
 
 		return self.__redis.get(id)
 
+	def request(self, id, path):
+		worker = self.get(id)
+
+		if worker is None:
+			log.warn("Redis not ready")
+			raise LookupError()
+
+		response = httpx.post(urljoin(worker, path))
+		response.raise_for_status()
+
+		return response.json()
