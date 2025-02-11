771156d2f53532b04f66b0eb0667bc25f5a3708b
Michał Racinowski <m.racinowski@students.mimuw.edu.pl>
2025-02-11 15:04:44 +0100

    Clean up shard distribution code

--- manager/main.py
+++ manager/main.py
@@ -12,17 +12,14 @@ service = Service()
 
 @app.on_event("startup")
 async def startup():
-	service.connect(
-		os.environ['REDIS_SERVICE_HOST'],
-		os.environ['REDIS_SERVICE_PORT']
-	)
-
-
-@app.get("/")
-async def hello():
-	return "Hello world!"
+    service.connect(os.environ["REDIS_SERVICE_HOST"], os.environ["REDIS_SERVICE_PORT"])
 
 
 @app.post("/lease")
 async def lease(registration: Registration):
-	return service.lease(registration)
+    """Acquire or renew a lease for any shard
+
+    * **url**: Base URL of the requestee
+    * **renew**: If set, name of the shard to renew
+    """
+    return service.lease(registration)
--- worker/shard.py
+++ worker/shard.py
@@ -12,66 +12,53 @@ log = logging.getLogger("uvicorn")
 
 
 class Shard:
-	async def __try_lease(self):
-		registration = Registration(
-			url=self.__pod_url,
-			renew=None if self.__lease is None else self.__lease.name
-		)
+    async def __try_lease(self):
+        """Attempt to acquire a lease and return it if successful."""
+        registration = Registration(
+            url=self.__pod_url,
+            renew=None if self.__lease is None else self.__lease.name,
+        )
 
-		try:
-			response = httpx.post(
-				urljoin(self.__url, "lease"),
-				json=registration.model_dump()
-			)
-			response.raise_for_status()
+        try:
+            response = httpx.post(
+                urljoin(self.__url, "lease"), json=registration.model_dump()
+            )
+            response.raise_for_status()
 
-			return Lease.model_validate(response.json())
-		except HTTPError:
-			return None
-		except ValidationError:
-			return None
+            return Lease.model_validate(response.json())
+        except HTTPError:
+            return None
+        except ValidationError:
+            return None
 
-	def __init__(
-		self,
-		pod_host, pod_port,
-		manager_host, manager_port
-	):
-		self.__lease = None
-		self.__url = urlunsplit((
-			"http",
-			manager_host + ":" + manager_port,
-			"",
-			"",
-			""
-		))
-		self.__pod_url = urlunsplit((
-			"http",
-			pod_host + ":" + pod_port,
-			"",
-			"",
-			""
-		))
+    def __init__(self, pod_host, pod_port, manager_host, manager_port):
+        self.__lease = None
+        self.__url = urlunsplit(("http", manager_host + ":" + manager_port, "", "", ""))
+        self.__pod_url = urlunsplit(("http", pod_host + ":" + pod_port, "", "", ""))
 
-	async def lease(self, callback):
-		while True:
-			while self.__lease is None:
-				self.__lease = await self.__try_lease()
-				if self.__lease is None:
-					await sleep(TIMEOUT)
+    async def lease(self, callback):
+        """Manage shard leasing, calling callback on change; Does not return."""
+        while True:
+            while self.__lease is None:
+                self.__lease = await self.__try_lease()
+                if self.__lease is None:
+                    await sleep(TIMEOUT)
 
-			current = self.__lease.name
+            current = self.__lease.name
 
-			log.info("Acquired lease for {}".format(self.__lease))
+            log.info("Acquired lease for {}".format(self.__lease))
 
-			callback(self.__lease.name)
+            callback(self.__lease.name)
 
-			while self.__lease is not None and self.__lease.name == current:
-				await sleep(self.__lease.duration / 2.0)
-				self.__lease = await self.__try_lease()
+            while self.__lease is not None and self.__lease.name == current:
+                await sleep(self.__lease.duration / 2.0)
+                self.__lease = await self.__try_lease()
 
-			log.info("Lease lost for {}".format(current))
+            log.info("Lease lost for {}".format(current))
 
-	def lease_name(self):
-		if self.__lease is None:
-			return None
-		return self.__lease.name
+    def lease_name(self):
+        """Returns currently leased shard's name."""
+        if self.__lease is None:
+            return None
+
+        return self.__lease.name
